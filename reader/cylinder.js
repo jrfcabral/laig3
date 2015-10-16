85b2c0b30c04a0ce4d85a2f28fa33af53ebd8f67
João Cabral <joaofigueiredocabral@gmail.com>
2015-10-16 19:56:40 +0100

    Arranjados os cilindros

--- reader/cylinder.js
+++ reader/cylinder.js
@@ -36,7 +36,7 @@
 
 	for(var i = 0; i <= this.stacks; i++){
 		var radius = this.br + ((this.tr - this.br)/this.stacks)*i;
-		for(var j = 0; j < this.slices; j++){
+		for(var j = 0; j <= this.slices; j++){
 			var x = Math.cos(ang*j)*radius; 
 			var z = i * this.height/this.stacks;
 			var y = Math.sin(ang*j)*radius; 
@@ -45,22 +45,21 @@
 			this.normals.push(x, 0, z);
 			this.texCoords.push(j/this.slices, i/this.stacks);
 		}
-		this.texCoords.push((j+1)/this.slices, i/this.stacks);
+		
 	}
 
 	
 	for(var i = 0; i < this.stacks; i++){
-		for(var j = 0; j < this.slices-1; j++){
-			this.indices.push((i*this.slices) + this.slices+j, (i*this.slices) +j+1, (i*this.slices) +j);
-			this.indices.push((i*this.slices) +j, (i*this.slices) +j+1, (i*this.slices) + this.slices+j);
-			this.indices.push((i*this.slices) +j+1, (i*this.slices) +this.slices+j, (i*this.slices) +this.slices+j+1);
-			this.indices.push( (i*this.slices) +this.slices+j+1, (i*this.slices) +this.slices+j, (i*this.slices) +j+1);
+		for(var j = 0; j < this.slices; j++){
+			this.indices.push( (i*(this.slices+1))+j, i*(this.slices+1)+j+1,  (i+1)*(this.slices+1)  +j);
+			this.indices.push(i*(this.slices+1)+j+1,(i+1)*(this.slices+1)  +j+1 , (i+1)*(this.slices+1)  +j )
 		}
-		this.indices.push((i*this.slices) +this.slices+j, (i*this.slices) +0, (i*this.slices) +j);
+		/*this.indices.push((i*this.slices) +this.slices+j, (i*this.slices) +0, (i*this.slices) +j);
 		this.indices.push((i*this.slices) +j, (i*this.slices) +0, (i*this.slices) +this.slices+j);
 		this.indices.push((i*this.slices) +this.slices+j, ((i+1)*this.slices), (i*this.slices) +0);
-		this.indices.push( (i*this.slices) +0, ((i+1)*this.slices),(i*this.slices) +this.slices+j);
-		//console.log(this.indices);
+		this.indices.push( (i*this.slices) +0, ((i+1)*this.slices),(i*this.slices) +this.slices+j);*/
+		console.log(this.indices);
+		console.log(this.vertices);
 	}
 
 
