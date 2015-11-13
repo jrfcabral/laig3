0c126d1cc1022f34c8b5416985f27ff9c38b76c3
jrfcabral <joaofigueiredocabral@gmail.com>
2015-11-13 18:14:48 +0000

    coisas2

--- reader/terrain.js
+++ reader/terrain.js
@@ -1,26 +1,31 @@
-function terrain(scene, heightmap, texture){
-
-   	CGFobject.call(this,scene);
-
+function terrain(scene, heightmap, texture) {
+    
+    CGFobject.call(this, scene);
+    
     this.scene = scene;
     this.heightmap = new CGFtexture(scene,heightmap);
     this.textured = new CGFtexture(scene,texture);
-
-   
-    this.shader = new CGFshader(scene.gl, "shaders/heightmap.vert", "shaders/texture.frag");
-    this.shader.setUniformsValues({uSampler2:1,uSampler3:2});
-    this.plane = new Plane(this.scene, 200);
+    
+    
+    this.shader = new CGFshader(scene.gl,"shaders/heightmap.vert","shaders/texture.frag");
+    this.shader.setUniformsValues({
+        uSampler2: 1,
+        uSampler3: 2
+    });
+    this.plane = new Plane(this.scene,200);
 }
 
 terrain.prototype = Object.create(CGFobject.prototype);
 terrain.prototype.constructor = terrain;
 
-terrain.prototype.display = function(){
+terrain.prototype.display = function() {
     this.scene.setActiveShader(this.shader);
-     this.textured.bind(1);
+    this.textured.bind(1);
     this.heightmap.bind(2);
     this.plane.display();
     this.scene.setActiveShader(this.scene.defaultShader);
-};
+}
+;
 
-terrain.prototype.updateTexCoords = function(){};
+terrain.prototype.updateTexCoords = function() {}
+;
