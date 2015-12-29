dd515293d5a1162640e47b780f87eaa6a2b8d8a0
Areadrill <jackthemotorcycle@gmail.com>
2015-12-29 01:12:39 +0000

    more precise ghosts

--- reader/Ghost.js
+++ reader/Ghost.js
@@ -3,7 +3,8 @@ function Ghost(scene, id){
     this.x = Math.floor(id/1000);
     this.y = Math.floor(id % 1000)-1;
     this.rect = new rectangle(scene, [-0.5, 0.5],[0.5, -0.5]);
-    this.cyl = new cylinder(scene, 1.5, 0.4, 0.4, 40, 40);
+    this.cyl = new cylinder(scene, 1.1, 0.45, 0.25, 40, 40);
+    this.sph = new sphere(scene, 0.25, 40, 40);
     this.id = id;
 }
 
@@ -12,10 +13,16 @@ Ghost.prototype.display = function(){
         this.scene.pushMatrix();
         this.scene.registerForPick(this.id, this.rect);
         this.scene.registerForPick(this.id, this.cyl);
+        this.scene.registerForPick(this.id, this.sph);
         this.rect.display();
         //console.log(this.scene.board.board[this.x][this.y]);
-        if (this.scene.board.boardTxt[this.y][this.x] != 0)
+        if (this.scene.board.boardTxt[this.y][this.x] != 0){
             this.cyl.display();
+            this.scene.pushMatrix();
+            this.scene.translate(0, 0, 1.2);
+            this.sph.display();
+            this.scene.popMatrix();
+        }
         this.scene.clearPickRegistration();
         this.scene.popMatrix();
    }
